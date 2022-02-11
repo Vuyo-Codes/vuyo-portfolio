2ba1a21a179b9c7541019715f0f1c92f4a1c7740
Vuyo-Codes <97601729+Vuyo-Codes@users.noreply.github.com>
2022-02-11 13:12:45 +0200

    Update about.jsx

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -19,7 +19,7 @@ const About = () => {
               I am a very driven individual determined to gain as much working experience  within the I.T field, 
               as well as launching my own company in the near future based on the skills I have acquired. One of my best 
               traits would be the fact that I give 100% into everything I do and I work well with others. 
-              I believe I would be an asset to the Sovtech team if given the chance.  
+              I believe I would be an asset to any team if given the chance.  
               </p>
             
           </div>
